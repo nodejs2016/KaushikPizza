a0ab21eb4bb5dd4adb7ba402ee692e3b27002420
Babita Kaushik <babitakkaushik@gmail.com>
2016-11-02 20:01:38 -0400

    Added few files and modification

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var exphbs  = require('express-handlebars');
 var path = require('path');
 var fs = require('fs');
 var url = require('url');
+var jsonQuery = require('json-query')
+
 
 var app = express();
 
@@ -16,3 +18,33 @@ app.engine('hbs',hbs.engine);
 app.set('view engine', 'hbs');
 
 app.use(express.static(path.join(__dirname, '/static')));
+
+app.get('/', function(req,res){
+	var pizzaData = JSON.parse(fs.readFileSync('./data/pizza.json', 'utf8'));
+	console.log(pizzaData);
+    res.render('home',pizzaData);
+});
+
+app.get('/feature', function (req, res) {
+	var queryData = url.parse(req.url, true).query;
+	var selectedId = queryData.id;
+	console.log('selectedId : '+selectedId);
+	var pizzadata = JSON.parse(fs.readFileSync('./data/pizza.json', 'utf8'));
+	
+	var result = jsonQuery('pizza[*id='+selectedId +']', {data: pizzadata}).value
+	console.log(result);
+    res.render('feature',result[0]);
+});
+
+ app.get('/aboutus', function (req, res) {
+    res.render('aboutus');
+});
+
+
+app.get('/contactus', function (req, res) {
+    res.render('contactus');
+});
+
+app.listen(3300, function () {
+    console.log('express-handlebars example server listening on: 3300');
+});
