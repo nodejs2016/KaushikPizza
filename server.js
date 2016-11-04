e9701393d98c15b893bf5d084d525046f12e7dda
Babita Kaushik <babitakkaushik@gmail.com>
2016-11-04 10:09:01 -0400

    My Account page added

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var path = require('path');
 var fs = require('fs');
 var url = require('url');
 var jsonQuery = require('json-query')
+var sql = require('mssql'); 
 
 
 var app = express();
@@ -19,10 +20,40 @@ app.set('view engine', 'hbs');
 
 app.use(express.static(path.join(__dirname, '/static')));
 
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "nodeapp"
+});
+
+
 app.get('/', function(req,res){
 	var pizzaData = JSON.parse(fs.readFileSync('./data/pizza.json', 'utf8'));
 	console.log(pizzaData);
     res.render('home',pizzaData);
+});
+
+app.get('/authenticate', function(req,res){
+	var queryData = url.parse(req.url, true).query;
+	var username = queryData.username;
+	var password = queryData.password;
+	console.log('username ::' +username );
+	console.log('password ::' +password );
+	var userdata = JSON.parse(fs.readFileSync('./data/user.json', 'utf8'));
+	var result = jsonQuery('user[*username='+username +']', {data: userdata}).value
+	console.log(result);
+	if (result.length > 0 && result[0].password === password) {
+		res.render('myaccount',result[0]);
+	} else {
+
+				var pizzaData = JSON.parse(fs.readFileSync('./data/pizza.json', 'utf8'));
+				pizzaData.error = "your credentials are wrong";
+	console.log(pizzaData);
+    res.render('home',pizzaData);
+	}
+
 });
 
 app.get('/feature', function (req, res) {
@@ -34,6 +65,15 @@ app.get('/feature', function (req, res) {
 	var result = jsonQuery('pizza[*id='+selectedId +']', {data: pizzadata}).value
 	console.log(result);
     res.render('feature',result[0]);
+});
+
+app.get('/registration', function(req,res){
+	res.render('registration');
+});
+
+app.get('/signup', function(req,res){
+	res.render('signup');
+
 });
 
  app.get('/aboutus', function (req, res) {
